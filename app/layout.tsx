7f97b6bfcc53c8e9fa61a0b8424f6157596c8256
eorev <eorev@udel.edu>
2024-04-02 10:47:24 -0400

    added tags needed for insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className={cn("min-h-screen bg-background font-sans antialiased")}>
       {children}
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
