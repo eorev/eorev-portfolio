426e86d5670aae2701e81f12ff00b6ac27decbca
eorev <eorev@udel.edu>
2024-04-18 11:12:52 -0400

    added new page to nav, added component

---
+++ components/BrainToMe.tsx
@@ -0,0 +1,11 @@
+import "@/app/globals.css";
+
+const BrainToMe = () => {
+    return (
+        <div className="max-w-none min-w-0 pb-8 opacity-95 bg-background text-copy-light p-8 rounded-lg">
+            <h1 className="text-4xl font-bold my-2 text-primary">The 🧠 To Me</h1>
+        </div>
+    );
+}
+
+export default BrainToMe;
